49129b6b79a05f2fc105ce462eeb748b9ec18ef8
Wilquer82 <wilquer.fig@gmail.com>
2022-02-04 12:55:46 -0300

    Correção do arrayUsers2

--- sockets/chat.js
+++ sockets/chat.js
@@ -6,16 +6,16 @@ module.exports  = (io) => {
   io.on('connection', socket => {
     socket.on('saveNickname', nickName => {
       users.push(nickName);
-        io.emit('usersOn', users);
-    });
+      io.emit('usersOn', users);
   
-    socket.on('message', data => {
-      saveMessage(data);
-      io.emit('message', data);
+      socket.on('message', data => {
+        saveMessage(data);
+        io.emit('message', data);
       
-      socket.on('disconnect', () => {
-        let usersActual = users.filter((item) => item !== nickName);
-        socket.broadcast.emit('usersOn', usersActual);
+        socket.on('disconnect', () => {
+          let usersActual = users.filter((item) => item !== nickName);
+          socket.broadcast.emit('usersOn', usersActual);
+        })
       })
     })
   })
