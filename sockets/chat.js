5ac0d02f9dc39bf1916eb05b1607258e379daa0d
Wilquer82 <wilquer.fig@gmail.com>
2022-02-04 13:35:09 -0300

    Correção do Array ....

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,22 +1,21 @@
 const { saveMessage } = require('../controller/chatController');
 
+let users = [];
+
 module.exports = (io) => {
-  let users = [];
   io.on('connection', socket => {
     socket.on('saveNickname', nickName => {
       users.push(nickName);
       io.emit('usersOn', users);
-  
+      })
       socket.on('message', data => {
         saveMessage(data);
         io.emit('message', data);
-      
-        socket.on('disconnect', () => {
-          let usersActual = users.filter((item) => item !== nickName);
-          console.log(usersActual);
-          socket.emit('usersOn', usersActual);
-        })
       })
-    })
+      socket.on('disconnect', nickName => {
+        let usersActual = users.filter((item) => item !== nickName);
+        console.log(usersActual);
+        io.emit('usersOn', usersActual);
+      })
   })
 }
